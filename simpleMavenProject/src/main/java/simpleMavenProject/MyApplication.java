158754710f8ec5af83a6ff31c827d8f61e089bfc
Ayman <abqary.ayman@gmail.com>
2017-12-24 16:03:58 +0000

    init

--- simpleMavenProject/src/main/java/simpleMavenProject/MyApplication.java
+++ simpleMavenProject/src/main/java/simpleMavenProject/MyApplication.java
@@ -3,6 +3,8 @@ package simpleMavenProject;
 public class MyApplication {
 	public String myMethod (){
 		System.out.println("Hello world");
+		System.out.println("Hello From Ranya,Mokhless,Ayman");
+		System.out.println("!!!!");
 		return "Ok";
 	}
 	
